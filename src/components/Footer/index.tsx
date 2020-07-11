a9dd8de26a7b1af1314be6abc609454dff93af38
Evandro Ribeiro <ribeiroevandro@live.com>
2020-07-11 16:06:36 -0300

    chore: Separating responsibility from footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <S.Footer>
+      Feito com ❤️ por Helena Paixão 👋️{" "}
+      <a href="https://www.linkedin.com/in/helenapaixao">
+        Entre em contato!
+      </a>
+    </S.Footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { shade } from "polished";
+
+export const Footer = styled.footer`
+  grid-area: footer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    text-decoration: none;
+    margin-left: 10px;
+    color: #3a3a3a;
+    font-weight: bold;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      color: ${shade(0.2, "#3A3A3A")};
+    }
+  }
+`;
