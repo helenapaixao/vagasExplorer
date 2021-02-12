835a39a4bfb24613a2e98f3f7a0b26ccecf96a0a
Helena Paixão <hp.helenapaixao@gmail.com>
2021-02-12 11:34:40 -0300

    feat: create styles search and add page in routes

---
+++ src/pages/Search/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+
+
+
+ export const Container = styled.div`
+
+
+
+`;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
+import Search from '../pages/Search';
 import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/dashboard" component={Dashboard} />
+    <Route exact path="search" component={Search} />
     <Route path="/repository/:repository+" component={Repository} />
   </Switch>
 );
