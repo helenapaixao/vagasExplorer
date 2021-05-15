db6ac514ea7426d4f3dabb7be2a921713deaec79
rawallon <rawallon@gmail.com>
2021-05-14 22:02:30 -0300

    Moved repos  to an array

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,6 +7,67 @@ import Header from '../../components/Header';
 import * as S from './styles';
 import { ToggleTheme } from '../../utils/ToggleThemeInterface';
 
+const reposData = [
+  {
+    link: '/repository/backend-br/vagas',
+    imageUrl: 'https://avatars1.githubusercontent.com/u/30732658?v=4',
+    name: ' backend-br',
+    desc: 'Espaço para divulgação de vagas para backenders',
+  },
+  {
+    link: '/repository/frontendbr/vagas',
+    imageUrl: 'https://avatars1.githubusercontent.com/u/16963863?v=4',
+    name: ' frontendbr',
+    desc: 'Espaço para divulgação de vagas para front-enders.',
+  },
+  {
+    link: '/repository/vuejs-br/vagas',
+    imageUrl:
+      'https://avatars2.githubusercontent.com/u/13300590?v=4.githubusercontent.com/u/16963863?v=4',
+    name: ' vuejs-br',
+    desc: 'Espaço para divulgação de vagas relacionadas com Vue.js',
+  },
+  {
+    link: '/repository/androiddevbr/vagas',
+    imageUrl: 'https://avatars1.githubusercontent.com/u/13825651?v=4',
+    name: ' androiddevbr',
+    desc: 'Mural de vagas para desenvolvedor Android.',
+  },
+  {
+    link: '/repository/react-brasil/vagas',
+    imageUrl: 'https://avatars2.githubusercontent.com/u/16929016?s=500&v=4',
+    name: ' React Brasil',
+    desc: 'Espaço para divulgação de vagas relacionadas com React',
+  },
+  {
+    link: '/repository/qa-brasil/vagas',
+    imageUrl: 'https://avatars0.githubusercontent.com/u/59667653?s=200&v=4',
+    name: ' QA Brasil ',
+    desc: 'Espaço para divulgação de vagas para Analistas de testes',
+  },
+  {
+    link: '/repository/phpdevbr/vagas',
+    imageUrl:
+      'https://avatars0.githubusercontent.com/u/21205969?v=3&u=811926aba01e8a43d7a8ffda50b7b66a57ccdd0a',
+    name: ' phpdevbr',
+    desc: 'Espaço para divulgação de vagas para desenvolvedores PHP.',
+  },
+  {
+    link: '/repository/flutterbr/vagas',
+    imageUrl:
+      'https://github.com/Flutter-Brazil/brand/raw/master/src/jpg/banner.jpeg',
+    name: ' flutterbr',
+    desc: ' Espaço para divulgação de vagas relacionadas com Flutter e Dart',
+  },
+  {
+    link: '/repository/uxbrasil/vagas',
+    imageUrl:
+      'https://user-images.githubusercontent.com/3299130/48214486-2fed0800-e367-11e8-8274-6858c79ab7b4.png',
+    name: ' uxbrasil',
+    desc: ' Espaço para divulgação de vagas para designers UI e pesquisadores UX.',
+  },
+];
+
 const Dashboard: React.FC<ToggleTheme> = ({ toggleTheme }) => {
   return (
     <Layout isContentFull>
@@ -16,137 +77,16 @@ const Dashboard: React.FC<ToggleTheme> = ({ toggleTheme }) => {
         <h1>Principais repositórios de vagas</h1>
 
         <S.Content>
-          <S.RepositoryItem
-            key="backend-br/vagas"
-            to="/repository/backend-br/vagas"
-          >
-            <img
-              src="https://avatars1.githubusercontent.com/u/30732658?v=4"
-              alt="backend-br"
-            />
-            <div>
-              <strong>backend-br</strong>
-              <p>Espaço para divulgação de vagas para backenders</p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="frontendbr/vagas"
-            to="/repository/frontendbr/vagas"
-          >
-            <img
-              src="https://avatars1.githubusercontent.com/u/16963863?v=4"
-              alt="frontendbr"
-            />
-            <div>
-              <strong>frontendbr</strong>
-              <p>Espaço para divulgação de vagas para front-enders.</p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="vuejs-br/vagas"
-            to="/repository/vuejs-br/vagas"
-          >
-            <img
-              src="https://avatars2.githubusercontent.com/u/13300590?v=4.githubusercontent.com/u/16963863?v=4"
-              alt="vuejs-br"
-            />
-            <div>
-              <strong>vuejs-br</strong>
-              <p>Espaço para divulgação de vagas relacionadas com Vue.js</p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="androiddevbr/vagas"
-            to="/repository/androiddevbr/vagas"
-          >
-            <img
-              src="https://avatars1.githubusercontent.com/u/13825651?v=4"
-              alt="androiddevbr-br"
-            />
-            <div>
-              <strong>androiddevbr</strong>
-              <p>Mural de vagas para desenvolvedor Android.</p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="react-brasil/vagas"
-            to="/repository/react-brasil/vagas"
-          >
-            <img
-              src="https://avatars2.githubusercontent.com/u/16929016?s=500&v=4"
-              alt="React Brasil"
-            />
-            <div>
-              <strong>React Brasil</strong>
-              <p>Espaço para divulgação de vagas relacionadas com React</p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="qa-brasil/vagas"
-            to="/repository/qa-brasil/vagas"
-          >
-            <img
-              src="https://avatars0.githubusercontent.com/u/59667653?s=200&v=4"
-              alt="@qa-brasil"
-            />
-            <div>
-              <strong>QA Brasil </strong>
-              <p>Espaço para divulgação de vagas para Analistas de testes</p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="phpdevbr/vagas"
-            to="/repository/phpdevbr/vagas"
-          >
-            <img
-              src="https://avatars0.githubusercontent.com/u/21205969?v=3&u=811926aba01e8a43d7a8ffda50b7b66a57ccdd0a"
-              alt="@qa-brasil"
-            />
-            <div>
-              <strong>phpdevbr</strong>
-              <p>Espaço para divulgação de vagas para desenvolvedores PHP.</p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="flutterbr/vagas"
-            to="/repository/flutterbr/vagas"
-          >
-            <img
-              src="https://github.com/Flutter-Brazil/brand/raw/master/src/jpg/banner.jpeg"
-              alt="@flutter-Brazil"
-            />
-            <div>
-              <strong>flutterbr</strong>
-              <p>
-                Espaço para divulgação de vagas relacionadas com Flutter e Dart
-              </p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
-          <S.RepositoryItem
-            key="uxbrasil/vagas"
-            to="/repository/uxbrasil/vagas"
-          >
-            <img
-              src="https://user-images.githubusercontent.com/3299130/48214486-2fed0800-e367-11e8-8274-6858c79ab7b4.png"
-              alt="@uxbrasil"
-            />
-            <div>
-              <strong>uxbrasil</strong>
-              <p>
-                Espaço para divulgação de vagas para designers UI e
-                pesquisadores UX.
-              </p>
-            </div>
-            <FiChevronRight size={20} />
-          </S.RepositoryItem>
+          {reposData.map((repo) => (
+            <S.RepositoryItem key={repo.link} to={repo.link}>
+              <img src={repo.imageUrl} alt={repo.name} />
+              <div>
+                <strong>{repo.name}</strong>
+                <p>{repo.desc}</p>
+              </div>
+              <FiChevronRight size={20} />
+            </S.RepositoryItem>
+          ))}
         </S.Content>
       </S.Repositories>
     </Layout>
