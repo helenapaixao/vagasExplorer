7683e074cdc382d18e42e8ae6f83dd618a6409e1
helenapaixao <hp.helenapaixao@gmail.com>
2025-07-27 19:02:33 -0300

    fix(eslint): codig arrumado

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -2,11 +2,11 @@ const dark = {
   title: 'dark',
 
   colors: {
-    primary: '#8257e6',     // roxo
-    secundary: '#04d361',   // verde
+    primary: '#8257e6', // roxo
+    secundary: '#04d361', // verde
     background: '#3c3744',
-    text: '#ffffff'
-  }
+    text: '#ffffff',
+  },
 };
 
 export default dark;
